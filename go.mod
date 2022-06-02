6828bec73a93127b5d14235c98eb74216dcc6d57
João Vitor <jv.galvao14@gmail.com>
2022-06-01 22:17:48 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/king-gizzard-api
+
+go 1.18
---
+++ index.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Albums struct {
+	StudioAlbums []Album `json:"studioAlbums"`
+	LiveAlbums   []Album `json:"liveAlbums"`
+	WeirdStuff   []Album `json:"weirdStuff"`
+}
+
+type Album struct {
+	ReleaseDate string `json:"releaseDate"`
+	Name        string `json:"name"`
+	Art         string `json:"art"`
+	Player      string `json:"player"`
+	Label       string `json:"label"`
+}
+
+var AllAlbums Albums
+
+func returnAllAlbums(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllAlbums")
+	json.NewEncoder(w).Encode((AllAlbums))
+}
+
+func returnStudioAlbums(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnStudioAlbums")
+	json.NewEncoder(w).Encode((AllAlbums.StudioAlbums))
+}
+
+func returnLiveAlbums(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnLiveAlbums")
+	json.NewEncoder(w).Encode((AllAlbums.LiveAlbums))
+}
+
+func returnWeirdStuff(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnWeirdStuff")
+	json.NewEncoder(w).Encode((AllAlbums.WeirdStuff))
+}
+
+func handleRequests() {
+	http.HandleFunc("/albums", returnAllAlbums)
+	http.HandleFunc("/albums/studio", returnStudioAlbums)
+	http.HandleFunc("/albums/live", returnLiveAlbums)
+	http.HandleFunc("/albums/weird", returnWeirdStuff)
+	log.Fatal(http.ListenAndServe(":3302", nil))
+}
+
+func main() {
+
+	jsonFile, err := os.Open("album-data.json")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	byteValue, _ := ioutil.ReadAll((jsonFile))
+
+	json.Unmarshal(byteValue, &AllAlbums)
+
+	println("API Running")
+
+	handleRequests()
+}
