ef12f98cf6151548691bf56e11c3444ea25dcfe4
João Vitor <jv.galvao14@gmail.com>
2022-06-01 22:44:57 -0300

    chore: add godotevn

--- index.go
+++ index.go
@@ -62,8 +62,6 @@ func handleRequests() {
 
 func main() {
 
-	// load .env file from given path
-	// we keep it empty it will load .env from current directory
 	err := godotenv.Load(".env")
 
 	if err != nil {
